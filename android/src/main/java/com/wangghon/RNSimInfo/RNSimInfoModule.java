3b6e9fbda1e6bce64fa1cb7e81d1eddaa58c9f90
Hongbo Wang <hongbo.wang@salmat.com.au>
2017-07-04 09:56:05 +1000

    bug fix

--- android/src/main/java/com/wangghon/RNSimInfo/RNSimInfoModule.java
+++ android/src/main/java/com/wangghon/RNSimInfo/RNSimInfoModule.java
@@ -7,28 +7,29 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 import android.telephony.TelephonyManager;
+import android.content.Context;
 
 public class RNSimInfoModule extends ReactContextBaseJavaModule {
-
-  private final ReactApplicationContext reactContext;
-
-  public RNSimInfoModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNSimInfo";
-  }
-
-
-  @ReactMethod
-  public void isSimCardValid(Callback callback) {
-
-    TelephonyManager tm = (TelephonyManager) this.reactContext.getSystemService(Context.TELEPHONY_SERVICE);  //gets the current TelephonyManager
-    Bool isSimCardValid = tm.getSimState() == TelephonyManager.SIM_STATE_READY);
-
-    callback.invoke(isSimCardValid);
-  }
+    
+    private final ReactApplicationContext reactContext;
+    
+    public RNSimInfoModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+    
+    @Override
+    public String getName() {
+        return "RNSimInfo";
+    }
+    
+    
+    @ReactMethod
+    public void isSimCardValid(Callback callback) {
+        
+        TelephonyManager tm = (TelephonyManager) this.reactContext.getSystemService(Context.TELEPHONY_SERVICE);  //gets the current TelephonyManager
+        Boolean isSimCardValid = (tm.getSimState() == TelephonyManager.SIM_STATE_READY);
+        
+        callback.invoke(isSimCardValid);
+    }
 }
